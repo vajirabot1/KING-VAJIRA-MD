e84d04bbc625fd22b0c525234b21deb4c9f38f3c
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2024-01-03 11:45:47 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- media/image_gist.js
+++ media/image_gist.js
@@ -1,9 +1,13 @@
 {
-"hi": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/hi.mp3?raw=true",
-"bye": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/Bs(tbg).mp3?raw=true",
-"gn": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/Gn(tbg).mp3?raw=true",
-"gm": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/GM(tbg).mp3?raw=true",
-"bs": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/Bs(tbg).mp3?raw=true",
-"mk": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/mk.mp3?raw=true",
-"balli": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_voice/wesi(tbg).mp3?raw=true"
-  }
+"hi": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_image/hi.jpg?raw=true",
+"wesigeputha": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_image/wesigeputha.jpg?raw=true",
+"wesa": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_image/wesa.jpg?raw=true",
+"ponnaya": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_image/ponnaya.jpg?raw=true",
+"pakaya": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_image/pakaya.jpg?raw=true",
+"huththi": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_image/huththi.jpg?raw=true",
+"huththa": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_image/huththa.jpg?raw=true",
+"hukanna": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_image/hukanna.jpg?raw=true",
+"huththigeputha": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_image/hiththigeputha.jpg?raw=true",
+"mk": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_image/mk.jpg?raw=true",
+"kariya": "https://github.com/vajirabot1/KING-VAJIRA-MD/blob/main/media/auto_image/kariya.jpg?raw=true"
+}
