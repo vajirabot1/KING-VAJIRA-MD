d0b8b7a08ab9d6737f4f98aa2ba53c0085d5c412
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2024-01-03 11:49:44 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/auto_image_sender.js
+++ commands/auto_image_sender.js
@@ -2,7 +2,7 @@ const { tlang,cmd } = require('../lib')
 const Config = require('../config')
 const axios = require('axios')
 // Put here your url with modified words and urls
-const url = 'https://gist.github.com/vajirabot1/8408dce754dc57efaecbdd100ac93ddd/raw'
+const url = 'https://gist.github.com/vajirabot1/9dd585014e2742484992c93d008531d4/raw'
 
 /**
  cmd({
